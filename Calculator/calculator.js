9be5b3c0d7e1efd3d2b10be8ecb147e91d804c8f
Victor Agbeo <75548415+saintvic@users.noreply.github.com>
2022-04-18 19:39:44 -0500

    BMI calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -11,8 +11,6 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-
-
 app.post("/", function(req, res){
     var num1 = Number(req.body.num1);
 // req.body.num1 returns a string so Number() changes it to a number
@@ -21,6 +19,31 @@ app.post("/", function(req, res){
     var result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
+});
+
+
+
+
+app.get("/bmiCalculator", function(req, res){
+
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmiCalculator", function(req, res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight/(height * height);
+
+    if(bmi < 18.5){
+        res.send("Your BMI is " + bmi + " , so you are underweight.");
+        
+    } 
+    if(bmi >= 18.5 && bmi < 24.9 ){
+        res.send("Your BMI is " + bmi + " , so you have a normal weight.");
+    } 
+
+    res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function(){
